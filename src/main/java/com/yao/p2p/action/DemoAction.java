824f7c3e0b8ff8732772127e988642713afcc5f4
yaohuanhuan <1107129170@qq.com>
2017-10-26 22:05:11 +0800

    加入hibernate

--- src/main/java/com/yao/p2p/action/DemoAction.java
+++ src/main/java/com/yao/p2p/action/DemoAction.java
@@ -1,10 +1,15 @@
 package com.yao.p2p.action;
 
 import com.opensymphony.xwork2.ActionSupport;
+import com.yao.p2p.b.User;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 /**
  * Created by yx on 2017/10/26.
@@ -13,17 +18,40 @@ import org.apache.struts2.convention.annotation.Result;
 
 @Namespace("/actions")
 @ParentPackage("struts-default")
-public class DemoAction  extends ActionSupport{
+public class DemoAction extends ActionSupport {
 
-    @Action(value = "sayHello",results = {
-            @Result(name = "ok" , location = "/test2.html"),
-            @Result(name = "no" , location = "/test.html")
+    @Action(value = "sayHello", results = {
+            @Result(name = "ok", location = "/test2.html"),
+            @Result(name = "no", location = "/test.html")
     })
-    public String sayHello(){
+    public String sayHello() {
+        User user = new User();
+        user.setUsername("伟哥哥");
+        user.setPassword("1234");
 
-        if (true){
+
+        //1 加载配置文件获得核心配置对象
+        Configuration config = new Configuration().configure();
+        //2 获得工厂 SessionFactory，相当于连接池
+        SessionFactory factory = config.buildSessionFactory();
+        //3获得会话session，相当于链接Connection
+        Session session = factory.openSession();
+        //4 开启事务
+        Transaction transaction = session.beginTransaction();
+
+        //操作
+        session.save(user);
+
+        //5 提交事务 | 回滚事务
+        transaction.commit();
+        //6 释放资源--关闭session
+        session.close();
+        //7 释放资源--关闭工厂factory
+        factory.close();
+
+        if (true) {
             return "ok";
-        }else {
+        } else {
             return "no";
         }
     }
--- src/main/java/com/yao/p2p/b/User.java
+++ src/main/java/com/yao/p2p/b/User.java
@@ -6,15 +6,35 @@ import org.springframework.stereotype.Component;
  * Created by yx on 2017/10/26.
  * hello
  */
-@Component
-public class User {
-    private int age;
 
-    public int getAge() {
-        return age;
+public class User {
+
+    private Integer id;
+    private String username;
+    private String password;
+
+
+    public Integer getId() {
+        return id;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
